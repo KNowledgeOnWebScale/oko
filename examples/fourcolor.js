4842956bae7d9df8fbedade80a14abaf79e4d8d9
josd <josderoo@gmail.com>
2022-02-06 20:27:46 +0100

    adding fourcolor example

---
+++ examples/fourcolor.js
@@ -0,0 +1,29 @@
+// Import Tau Prolog core
+var pl = require("tau-prolog");
+
+// Import the lists module
+require("tau-prolog/modules/lists")(pl);
+
+// Create a session
+var session = pl.create(10000);
+
+// Program
+var fs = require("fs");
+var program = fs.readFileSync("../webeye.pl");
+program += fs.readFileSync("./fourcolor.pl");
+
+// Goal
+var goal = "run([]).";
+
+// Load the program
+session.consult(program, {
+	success: function() {
+		// Query the goal
+		session.query(goal, {
+			success: function() {
+				// Look for answers
+				session.answers(x => null);
+			}
+		});
+	}
+});
